915b92c59446ab206ddbbbdab71d0399e5d5cd31
Andres Araque <aaraqueamaya397@gmail.com>
2025-02-04 12:43:25 -0500

    [ADD] Agregando productos al carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -9,6 +9,12 @@ const Card = (data) => {
     context.setProductToShow(productDetail);
   };
 
+  const addProductsToCart = (productData) => {
+    context.setCount(context.count + 1);
+    context.setCartProducts([...context.cartProducts, productData]);
+    console.log(context.cartProducts);
+  };
+
   return (
     <div className='bg-white cursor-pointer w-56 h-60 rounded-lg' onClick={() => showProduct(data.data)}>
       <figure className='relative mb-2 w-full h-4/5'>
@@ -18,7 +24,7 @@ const Card = (data) => {
         <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]} alt={data.data.title} />
         <div
           className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-          onClick={() => context.setCount(context.count + 1)}
+          onClick={() => addProductsToCart(data.data)}
         >
           <svg
             xmlns='http://www.w3.org/2000/svg'
