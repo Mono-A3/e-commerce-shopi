3c4a0e379e73d961d6a28c3e1e7dd13c5f4fba5b
Andres Araque <aaraqueamaya397@gmail.com>
2025-05-05 20:52:35 -0500

    [ADD] Crear el estado local para cambiar las vistas a renderizar

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,10 +1,11 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ShoppingCartContext } from '../../Context';
 import Layout from '../../Components/Layout';
 
 function SignIn() {
   const context = useContext(ShoppingCartContext);
+  const [view, setView] = useState('user-info');
 
   //Account
   const account = localStorage.getItem('account');
@@ -15,10 +16,8 @@ function SignIn() {
   const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true;
   const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
 
-  return (
-    <Layout>
-      <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
-
+  const renderLogIn = () => {
+    return (
       <div className='flex flex-col w-80'>
         <p>
           <span className='font-light text-sm'>Email: </span>
@@ -49,6 +48,17 @@ function SignIn() {
           Sign up
         </button>
       </div>
+    );
+  };
+
+  const renderCreateUserInfo = () => {};
+
+  const renderView = () => (view === 'create-user-info' ? renderCreateUserInfo() : renderLogIn());
+
+  return (
+    <Layout>
+      <h1 className='font-medium text-xl text-center mb-6 w-80'>Welcome</h1>
+      {renderView()}
     </Layout>
   );
 }
