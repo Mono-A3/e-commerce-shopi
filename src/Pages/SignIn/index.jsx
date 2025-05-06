c4e877757160807117353425be2226e1d7c3a1fa
Andres Araque <aaraqueamaya397@gmail.com>
2025-05-05 19:58:56 -0500

    [ADD] Leer account desde LocalStorage y desde el estado local para saber si el usuario tiene cuenta o no

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,4 +1,20 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ShoppingCartContext } from '../../Context';
+import Layout from '../../Components/Layout';
+
 function SignIn() {
+  const context = useContext(ShoppingCartContext);
+
+  //Account
+  const account = localStorage.getItem('account');
+  const parsedAccount = JSON.parse(account);
+
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0 : true;
+  const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0 : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
   return (
     <div>
       <h1>SignIn</h1>
