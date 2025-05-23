74c6b981f873f295cc1ea39b365f810e796385a5
Andres Araque <aaraqueamaya397@gmail.com>
2025-05-23 13:12:44 -0500

    [ADD] Importarlo y renderizarlo en Navbar

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { ShoppingCartContext } from '../../Context';
+import ShoppingCart from '../ShoppingCart';
 
 const Navbar = () => {
   const context = useContext(ShoppingCartContext);
@@ -139,22 +140,9 @@ const Navbar = () => {
       </ul>
       <ul className='flex items-center gap-3'>
         {renderView()}
+
         <li className='flex items-center cursor-pointer'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-            strokeWidth={1.5}
-            stroke='currentColor'
-            className='size-6'
-            onClick={() => context.openCheckoutSideMenu()}
-          >
-            <path
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
-            />
-          </svg>
+          <ShoppingCart />
           <div>{context.cartProducts.length}</div>
         </li>
       </ul>
--- src/Components/ShoppingCart/index.jsx
+++ src/Components/ShoppingCart/index.jsx
@@ -19,7 +19,11 @@ const ShoppingCart = () => {
         stroke='currentColor'
         className='w-6 h-6 fill-none stroke-black cursor-pointer'
       >
-        <path strokeLinecap='round' strokeLinejoin='round' d='m4.5 12.75 6 6 9-13.5' />
+        <path
+          strokeLinecap='round'
+          strokeLinejoin='round'
+          d='M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z'
+        />
       </svg>
 
       <div className='absolute bottom-3.5 left-3.5 flex justify-center items-center rounded-full bg-black w-4 h-4 text-xs text-white'>
